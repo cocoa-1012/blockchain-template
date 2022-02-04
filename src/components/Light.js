c76b85a218e02f471362744dbaee8f9125846bbb
cocoa-1012 <cocoa.pandaok@gmail.com>
2022-02-04 07:18:27 +0000

    light size modification

--- src/components/Light.js
+++ src/components/Light.js
@@ -1,4 +1,3 @@
-// import react from "react";
 import { motion } from "framer-motion";
 
 export const Light = ({ crdX, crdY, duration }) => {
@@ -6,17 +5,21 @@ export const Light = ({ crdX, crdY, duration }) => {
   return (
     <div className={`relative mt-4 ml-48`}>
       <div
-        className={`absolute w-[2px] h-[100px] bg-[#56bdcf] bottom-0 right-2`}
+        className={`absolute w-[2px] h-[100px] bg-[#56bdcf] bottom-0 right-1`}
       >
         <p className="absolute text-[#72f2ff] top-[-30px] left-[-10px] font-bold text-xl tracking-[-0.1em]">
           <span className="font-normal">&#x25EF;&nbsp;&nbsp;</span>25.01
         </p>
       </div>
       <motion.div
-        className={`bg-gradient-radial drop-shadow-[0_0_0_rgba(44,253,254,1)] w-[16px] h-[10px] rounded-[50%]`}
+        className={`bg-gradient-radial drop-shadow-[0_0_6px_#00fbfe] w-[12px] h-[8px] rounded-[50%]`}
         animate={{
-          scale: [1, 1.8, 1],
-          boxShadow: ["0 0 10px white", "0 0 20px white", "0 0 10px white"],
+          scale: [1, 1.6, 1],
+          boxShadow: [
+            "0 0 10px #00fbfe",
+            "0 0 20px #00fbfe",
+            "0 0 10px #00fbfe",
+          ],
         }}
         transition={{
           duration: 3,
@@ -27,11 +30,5 @@ export const Light = ({ crdX, crdY, duration }) => {
         }}
       />
     </div>
-
-    // <div className="w-[80px] h-[80px] bg-red-400">
-    //   <div className="bg-gradient-radial shadow-white w-[20px] h-[20px] rounded-[30px]">
-    //     <motion.div animate={{ scale: 2 }} transition={{ duration: 0.5 }} />
-    //   </div>
-    // </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial":
-          "radial-gradient(rgba(255,255,255,1),rgba(255,255,255,1),rgba(255,255,255,1), rgba(255,255,255,1),rgba(150,255,252,1),rgba(100,255,252,1),rgba(50,255,255,1),rgba(25,255,255,1),rgba(10,255, 255,1))",
+          "radial-gradient(rgba(255,255,255,1),rgba(255,255,255,1),rgba(255,255,255,1), rgba(255,255,255,1),rgba(200,255,255,1),rgba(100,255,255,1),rgba(50,255,255,1),rgba(150,255,255,1),rgba(10,255, 255,1))",
         "hero-pattern": "url('./assets/images/background.jpg')",
       },
     },
